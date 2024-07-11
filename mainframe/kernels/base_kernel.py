86a9f04f54f5442f5c1c9b90dc15ccf017e2885d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-07-11 13:06:28 -0400

    FIX pylint W0107

--- mainframe/kernels/base_kernel.py
+++ mainframe/kernels/base_kernel.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 from typing import List, Dict
 
 class BaseKernel(ABC):
+    """
+    This kernel is inherited to maintain all the methods that are common to all kernels.
+    """
     def __init__(self, total_instances: int):
         self.total_instances = total_instances
         self.instances = []
@@ -9,19 +12,15 @@ class BaseKernel(ABC):
     @abstractmethod
     def initialize(self) -> None:
         """Initialize the kernel and create instances."""
-        pass
 
     @abstractmethod
     def run_task(self, task: Dict) -> None:
         """Run a task on this kernel."""
-        pass
 
     @abstractmethod
     def get_available_instances(self, count: int) -> List:
         """Get a list of available instances."""
-        pass
 
     @abstractmethod
     def shutdown(self) -> None:
         """Shutdown all instances of this kernel."""
-        pass
